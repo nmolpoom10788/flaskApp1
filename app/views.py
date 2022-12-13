04c99452e97f4df4e4c94624c08c5b38a2157ed7
nmolpoom10788 <nmolpoom10788@gmail.com>
2022-12-13 16:03:22 +0700

    lab04

--- app/views.py
+++ app/views.py
@@ -1,16 +1,14 @@
-from flask import jsonify
+from flask import jsonify, render_template
+import datetime
 from app import app
- 
- 
+  
 @app.route('/')
 def home():
     return "Flask says 'Hello world!'"
  
- 
 @app.route('/phonebook')
 def index():
     return app.send_static_file('phonebook.html')
- 
  
 # This route serves the dictionary d at the route /date
 @app.route("/api/data")
@@ -25,3 +23,25 @@ def data():
 @app.route('/lab02')
 def resume():
     return app.send_static_file('lab02_resume.html')
+
+
+@app.route('/lab03')
+def lab03_home():
+    return render_template('lab03/index.html',utc_dt=datetime.datetime.utcnow())
+
+@app.route('/lab03/about/')
+def lab03_about():
+    return render_template('lab03/about.html')
+
+@app.route('/lab03/comments/')
+def lab03_comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.']
+ 
+    return render_template('lab03/comments.html', comments=comments)
+
+@app.route('/lab04')
+def lab04_bootstrap():
+    return app.send_static_file('lab04_bootstrap.html')
