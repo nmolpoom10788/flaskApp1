b0b98fd039c96e7df37cf4a833458287f8733176
nmolpoom10788 <nmolpoom10788@gmail.com>
2022-12-20 14:48:32 +0700

    lab05

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'e773fc44127ba18a33ca7dcdfc69011c588cd2f22f8d080f'
  
 from app import views  # noqa
